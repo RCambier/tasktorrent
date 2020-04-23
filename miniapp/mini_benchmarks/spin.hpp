95ef955755484693fc9f515493abba433c1acfdb
Leopold Cambier <leopoldcambier@gmail.com>
2020-04-22 21:49:51 -0700

    Removing dead code

--- miniapp/mini_benchmarks/spin.hpp
+++
@@ -1,9 +0,0 @@
-#include "util.hpp"
-
-void spin_for_seconds(double time) {
-    auto t0 = ttor::wctime();
-    while(true) {
-    	auto t1 = ttor::wctime();
-        if( ttor::elapsed(t0, t1) >= time ) break;
-    }
-}
