03bf2cd7f6c20f67f1d130e8da5dab175fda40a9
Leopold Cambier <leopoldcambier@gmail.com>
2020-05-11 21:39:42 -0700

    More localized deps

--- miniapp/dense_cholesky/cholesky.cpp
+++ miniapp/dense_cholesky/cholesky.cpp
@@ -15,6 +15,7 @@
 #include <iostream>
 #include <map>
 #include <memory>
+#include <set>
 
 #include <mpi.h>
 
@@ -50,9 +51,20 @@ void cholesky(const int n_threads, const int verb, const int block_size, const i
     
     // Map tasks to ranks
     auto block_2_rank = [&](int i, int j) {
+        assert(i >= 0 && i < num_blocks);
+        assert(j >= 0 && j < num_blocks);
         int r = (j % npcols) * nprows + (i % nprows);
         assert(r >= 0 && r < n_ranks);
         return r;
+    };
+
+    const int rank_row = (rank % nprows);
+    const int rank_col = (rank / nprows);
+    auto block_2_rank_row = [&](int i, int j) {
+        return i % nprows;
+    };
+    auto block_2_rank_col = [&](int i, int j) {
+        return j % npcols;
     };
 
     // Map threads to ranks
@@ -225,12 +237,19 @@ void cholesky(const int n_threads, const int verb, const int block_size, const i
 
     // Send a potrf'ed pivot A(k,k) and trigger trsms below requiring A(k,k)
     auto am_trsm = comm.make_large_active_msg( 
-            [&](int& j, view<int>& is) {
-                for(auto& i: is) {
+            [&](int& j) {
+                // for(auto& i: is) {
+                //     trsm.fulfill_promise({i,j});
+                // }
+                int off = (nprows + rank_row - block_2_rank_row(j,j)) % nprows;
+                assert(off > 0); // Can't be me
+                assert(off < nprows);
+                for (int i = j + off; i < num_blocks; i += nprows) {
+                    assert(block_2_rank(i,j) == rank);
                     trsm.fulfill_promise({i,j});
                 }
             },
-            [&](int& j, view<int>& is) {
+            [&](int& j) {
                 return blocks[j+j*num_blocks]->data();
             });
 
@@ -246,31 +265,47 @@ void cholesky(const int n_threads, const int verb, const int block_size, const i
         })
         .set_fulfill([&](int j) { // Triggers all trsms on rows i > j, A[i,j]
             assert(block_2_rank(j,j) == rank);
-            map<int,vector<int>> fulfill;
-            for (int i = j+1; i<num_blocks; i++) {
-                fulfill[block_2_rank(i,j)].push_back(i);
-            }
-            for (auto& rf: fulfill) {
-                int r = rf.first;
-                if (rank == r) {
-                    for (auto& i: rf.second) {
-                        trsm.fulfill_promise({i,j});
-                        if(deps_log) {
-                            dlog.add_event(make_unique<DepsEvent>(potrf.name(j), trsm.name({i,j})));
-                        }
-                    }
-                } else {
-                    auto Ljjv = view<double>(blocks[j+j*num_blocks]->data(), block_size*block_size);
-                    auto isv = view<int>(rf.second.data(), rf.second.size());                    
-                    if(deps_log) {
-                        for(auto i: isv) {
-                            dlog.add_event(make_unique<DepsEvent>(potrf.name(j), trsm_name({i,j}, r)));
-                        }
-                    }
-                    am_trsm->send_large(r, Ljjv, j, isv);
-                }
 
+            // map<int,vector<int>> fulfill;
+            // for (int i = j+1; i<num_blocks; i++) {
+            //     fulfill[block_2_rank(i,j)].push_back(i);
+            // }
+            // for (auto& rf: fulfill) {
+            //     int r = rf.first;
+            //     if (rank == r) {
+            //         for (auto& i: rf.second) {
+            //             trsm.fulfill_promise({i,j});
+            //             if(deps_log) {
+            //                 dlog.add_event(make_unique<DepsEvent>(potrf.name(j), trsm.name({i,j})));
+            //             }
+            //         }
+            //     } else {
+            //         auto Ljjv = view<double>(blocks[j+j*num_blocks]->data(), block_size*block_size);
+            //         auto isv = view<int>(rf.second.data(), rf.second.size());                    
+            //         if(deps_log) {
+            //             for(auto i: isv) {
+            //                 dlog.add_event(make_unique<DepsEvent>(potrf.name(j), trsm_name({i,j}, r)));
+            //             }
+            //         }
+            //         am_trsm->send_large(r, Ljjv, j, isv);
+            //     }
+            // }
+
+            // Trigger myself
+            for (int i = j + nprows; i < num_blocks; i += nprows) {
+                assert(block_2_rank(i,j) == rank);
+                trsm.fulfill_promise({i,j});
             }
+            // Send to other procs in column
+            auto Ljjv = view<double>(blocks[j+j*num_blocks]->data(), block_size*block_size);
+            for(int p = 0; p < nprows; p++) {
+                if(j+p >= num_blocks) break;
+                int dest = block_2_rank(j+p,j);
+                if(dest != rank) {
+                    am_trsm->send_large(dest, Ljjv, j);
+                }
+            }
+
         })
         .set_indegree([&](int j) {
             assert(block_2_rank(j,j) == rank);
@@ -292,12 +327,32 @@ void cholesky(const int n_threads, const int verb, const int block_size, const i
 
     // Sends a panel (trsm'ed block A(i,j)) and trigger gemms requiring A(i,j)
     auto am_gemm = comm.make_large_active_msg(
-        [&](int& i, int& j, view<int2>& ijs) {
-            for(auto& ij: ijs) {
-                gemm.fulfill_promise({j,ij[0],ij[1]});
+        [&](int& i, int& j) {
+            // for(auto& ij: ijs) {
+            //     gemm.fulfill_promise({j,ij[0],ij[1]});
+            // }
+            if(block_2_rank_row(i,j) == rank_row) {
+                const int off_right = (npcols + rank_col - block_2_rank_col(i,j)) % npcols;
+                assert(off_right > 0); // Can't be me
+                assert(off_right < npcols);
+                assert(i >= 0 && i < num_blocks);
+                assert(j >= 0 && j < num_blocks);
+                for (int k = j + off_right; k < i; k += npcols) {
+                    assert(block_2_rank(i,k) == rank);
+                    gemm.fulfill_promise({j,i,k});
+                }
+            }
+            if(block_2_rank_col(i,i) == rank_col) {
+                const int off_below = (nprows + rank_row - block_2_rank_row(i,i)) % nprows;
+                assert(off_below >= 0); // Could be me
+                assert(off_below < nprows);
+                for (int k = i + off_below; k < num_blocks; k += nprows) {
+                    assert(block_2_rank(k,i) == rank);
+                    gemm.fulfill_promise({j,k,i});
+                }
             }
         },
-        [&](int& i, int& j, view<int2>& ijs) {
+        [&](int& i, int& j) {
             return blocks[i+j*num_blocks]->data();
         });
 
@@ -319,35 +374,70 @@ void cholesky(const int n_threads, const int verb, const int block_size, const i
             int j=ij[1];
             assert(block_2_rank(i,j) == rank);
             assert(i > j);
-            map<int,vector<int2>> fulfill;
-            for (int k = j+1; k < num_blocks; k++) {
-                int ii = std::max(i,k);
-                int jj = std::min(i,k);
-                fulfill[block_2_rank(ii,jj)].push_back({ii,jj});
+            // map<int,vector<int2>> fulfill;
+            // for (int k = j+1; k < num_blocks; k++) {
+            //     int ii = std::max(i,k);
+            //     int jj = std::min(i,k);
+            //     fulfill[block_2_rank(ii,jj)].push_back({ii,jj});
+            // }
+            // for (auto& rf: fulfill) {
+            //     int r = rf.first;
+            //     if (r == rank) {
+            //         for (auto& ij_gemm : rf.second) {
+            //             gemm.fulfill_promise({j,ij_gemm[0],ij_gemm[1]});
+            //             if(deps_log) {
+            //                 dlog.add_event(make_unique<DepsEvent>(trsm.name(ij), gemm.name({j,ij_gemm[0],ij_gemm[1]})));
+            //             }
+            //         }
+            //     }
+            //     else {
+            //         auto Lijv = view<double>(blocks[i+j*num_blocks]->data(), block_size*block_size);
+            //         auto ijsv = view<int2>(rf.second.data(), rf.second.size());
+            //         for(auto ij_gemm: ijsv) {
+            //             if(deps_log) {
+            //                 if(deps_log) {
+            //                     dlog.add_event(make_unique<DepsEvent>(trsm.name(ij), gemm_name({j,ij_gemm[0],ij_gemm[1]}, r)));
+            //                 }
+            //             }
+            //         }
+            //         am_gemm->send_large(r, Lijv, i, j, ijsv);
+            //     }
+            // }
+
+            // Local
+            // Careful to not count the pivot (syrk) twice
+            for (int k = j + npcols; k < i; k += npcols) {
+                assert(block_2_rank(i,k) == rank);
+                gemm.fulfill_promise({j,i,k});
             }
-            for (auto& rf: fulfill) {
-                int r = rf.first;
-                if (r == rank) {
-                    for (auto& ij_gemm : rf.second) {
-                        gemm.fulfill_promise({j,ij_gemm[0],ij_gemm[1]});
-                        if(deps_log) {
-                            dlog.add_event(make_unique<DepsEvent>(trsm.name(ij), gemm.name({j,ij_gemm[0],ij_gemm[1]})));
-                        }
-                    }
-                }
-                else {
-                    auto Lijv = view<double>(blocks[i+j*num_blocks]->data(), block_size*block_size);
-                    auto ijsv = view<int2>(rf.second.data(), rf.second.size());
-                    for(auto ij_gemm: ijsv) {
-                        if(deps_log) {
-                            if(deps_log) {
-                                dlog.add_event(make_unique<DepsEvent>(trsm.name(ij), gemm_name({j,ij_gemm[0],ij_gemm[1]}, r)));
-                            }
-                        }
-                    }
-                    am_gemm->send_large(r, Lijv, i, j, ijsv);
+            if(block_2_rank_col(i,i) == rank_col) {
+                int off_below = (nprows + rank_row - block_2_rank_row(i,i)) % nprows;
+                for (int k = i + off_below; k < num_blocks; k += nprows) {
+                    assert(block_2_rank(k,i) == rank);
+                    gemm.fulfill_promise({j,k,i});
                 }
             }
+            // Remote
+            auto Lijv = view<double>(blocks[i+j*num_blocks]->data(), block_size*block_size);
+            std::set<int> dests;
+            for (int c = 0; c < npcols; c++) {
+                if(j+c >= num_blocks) break;
+                int dest = block_2_rank(i,j+c);
+                if(dest != rank) {
+                    dests.insert(dest);
+                }
+            }
+            for (int r = 0; r < nprows; r++) {
+                if(i+r >= num_blocks) break;
+                int dest = block_2_rank(i+r,i);
+                if(dest != rank) {
+                    dests.insert(dest);
+                }
+            }
+            for(auto& dest: dests) {
+                am_gemm->send_large(dest, Lijv, i, j);
+            }
+
         })
         .set_indegree([&](int2 ij) {
             assert(block_2_rank(ij[0],ij[1]) == rank);
